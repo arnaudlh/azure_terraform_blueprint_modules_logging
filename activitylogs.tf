c2aae853d4147cd3aed2055664ca99412cc8f0c6
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2019-06-06 17:30:48 +0800

    Initial commit

---
+++ activitylogs.tf
@@ -0,0 +1,79 @@
+resource "random_string" "storageaccount_name" {
+    length  = 22
+    upper   = false
+    special = false
+}
+resource "azurerm_resource_group" "log" {
+  name      = "${var.resource_group_name}"
+  location  = "${var.location}"
+}
+
+resource "azurerm_storage_account" "log" {
+  name                     = "subscriptionlogs-${random_string.storageaccount_name.result}"
+  resource_group_name      = "${var.resource_group_name}"
+  location                 = "${var.location}"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_eventhub_namespace" "log" {
+  name                = "subscriptionlogs--${random_string.storageaccount_name.result}"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group_name}"
+  sku                 = "Standard"
+  capacity            = 2
+}
+resource "azurerm_monitor_log_profile" "subscription" {
+  name = "default"
+
+  categories = [
+    "Action",
+    "Delete",
+    "Write"
+  ]
+
+# Add all regions - > put in variable
+  locations = [
+    "eastus2",
+    "westus",
+    "centralus",
+    "centralindia",
+    "koreacentral",
+    "canadaeast",
+    "canadacentral",
+    "brazilsouth",
+    "francecentral",
+    "westcentralus",
+    "australiacentral",
+    "northcentralus",
+    "uksouth",
+    "francesouth",
+    "japanwest",
+    "global",
+    "southindia",
+    "australiaeast",
+    "ukwest",
+    "eastus",
+    "northeurope",
+    "australiacentral2",
+    "westeurope",
+    "westus2",
+    "southeastasia",
+    "eastasia",
+    "westindia",
+    "japaneast",
+    "southcentralus",
+    "australiasoutheast",
+    "koreasouth"
+  ]
+
+# RootManageSharedAccessKey is created by default with listen, send, manage permissions
+# servicebus_rule_id = "${var.eventhubnamespace_id}/authorizationrules/RootManageSharedAccessKey"
+# storage_account_id = "${azurerm_storage_account.test.id}"
+
+  retention_policy {
+    enabled = true
+    days    = 0 #infinite retention
+  }
+ 
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "resource_group_name" {
+  description = "(Required) Name of the resource group to deploy the KV"
+}
+
+variable "location" {
+  description = "(Required) Define the region where the resources will be created"
+}
